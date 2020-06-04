5d6b8b8c5354b507582fd784dfd5f398730f3f68
LoLz XVI <47308963+LoLzWTFomfg@users.noreply.github.com>
2020-06-03 23:42:36 -0500

    Basic elements implemented.
    Yet to implement functionality.

--- app/src/App.js
+++ app/src/App.js
@@ -2,9 +2,33 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [ display, setDisplay ] = React.useState('0')
   return (
     <div className="App">
-      HOLA
+
+      <div id='display'>{display}</div>
+
+      <div id='zero'>0</div>
+      <div id='one'>1</div>
+      <div id='two'>2</div>
+      <div id='three'>3</div>
+      <div id='four'>4</div>
+      <div id='five'>5</div>
+      <div id='six'>6</div>
+      <div id='seven'>7</div>
+      <div id='eight'>8</div>
+      <div id='nine'>9</div>
+
+      <div onClick={() => console.log('clicked')} id='add'>+</div>
+      <div onClick={() => console.log('clicked')} id='subtract'>-</div>
+      <div onClick={() => console.log('clicked')} id='multiply'>*</div>
+      <div onClick={() => console.log('clicked')} id='divide'>/</div>
+
+      <div id='clear'>C</div>
+      <div id='equals'>=</div>
+      <div id='decimal'>.</div>
+
+
     </div>
   );
 }
