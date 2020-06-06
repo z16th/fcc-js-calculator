45efccc98daff6f0fc7764950721c2a0b9c1da74
LoLz XVI <47308963+LoLzWTFomfg@users.noreply.github.com>
2020-06-06 03:42:54 -0500

    Implemented early style. Reduced main app code.

--- app/src/App.js
+++ app/src/App.js
@@ -1,34 +1,127 @@
 import React from 'react';
 import './App.css';
 
+const buttons = [
+  {
+    value: '0',
+    id: 'zero',
+    type: 'number'
+  },
+  {
+    value: '1',
+    id: 'one',
+    type: 'number'
+  },
+  {
+    value: '2',
+    id: 'two',
+    type: 'number'
+  },
+  {
+    value: '3',
+    id: 'three',
+    type: 'number'
+  },
+  {
+    value: '4',
+    id: 'four',
+    type: 'number'
+  },
+  {
+    value: '5',
+    id: 'five',
+    type: 'number'
+  },
+  {
+    value: '6',
+    id: 'six',
+    type: 'number'
+  },
+  {
+    value: '7',
+    id: 'seven',
+    type: 'number'
+  },
+  {
+    value: '8',
+    id: 'eight',
+    type: 'number'
+  },
+  {
+    value: '9',
+    id: 'nine',
+    type: 'number'
+  },
+  {
+    value: '+',
+    id: 'add',
+    type: 'operator'
+  },
+  {
+    value: '-',
+    id: 'subtract',
+    type: 'operator'
+  },
+  {
+    value: '*',
+    id: 'multiply',
+    type: 'operator'
+  },
+  {
+    value: '/',
+    id: 'divide',
+    type: 'operator'
+  },
+  {
+    value: 'C',
+    id: 'clear',
+    type: 'ui'
+  },
+  {
+    value: '=',
+    id: 'equals',
+    type: 'operator'
+  },
+  {
+    value: '.',
+    id: 'decimal',
+    type: 'operator'
+  },
+]
+
 function App() {
   const [ display, setDisplay ] = React.useState('0')
+
+  function handleClick(value){
+    if( value === '0' && display === '0') return
+    switch(value){
+      case 'C':
+        setDisplay('0')
+      break;
+      case '=':
+        setDisplay('Calculating')
+      break;
+      case '.':
+        if(display.slice(-1) === '.') return
+      //fall
+      default:
+        if(display === '0' && value !== '.') setDisplay(value)
+        else{
+          setDisplay(display + value)
+        }
+    }
+  }
+
   return (
     <div className="App">
 
       <div id='display'>{display}</div>
 
-      <div id='zero'>0</div>
-      <div id='one'>1</div>
-      <div id='two'>2</div>
-      <div id='three'>3</div>
-      <div id='four'>4</div>
-      <div id='five'>5</div>
-      <div id='six'>6</div>
-      <div id='seven'>7</div>
-      <div id='eight'>8</div>
-      <div id='nine'>9</div>
-
-      <div onClick={() => console.log('clicked')} id='add'>+</div>
-      <div onClick={() => console.log('clicked')} id='subtract'>-</div>
-      <div onClick={() => console.log('clicked')} id='multiply'>*</div>
-      <div onClick={() => console.log('clicked')} id='divide'>/</div>
-
-      <div id='clear'>C</div>
-      <div id='equals'>=</div>
-      <div id='decimal'>.</div>
-
-
+      <div id='pad'>
+        {buttons.map( ({value, id, type}) => (
+          <div key={id} id={id} className={`button ${type}`} onClick={() => handleClick(value)}>{value}</div>
+        ))}
+      </div>
     </div>
   );
 }
